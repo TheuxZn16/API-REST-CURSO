ee70ac65a88be241016f4903fdec8ff54bc1caed
Matheus Estevam <matheusestevam421@gmail.com>
2024-10-20 14:00:30 -0300

    aaaaa

--- dist/app.js
+++ dist/app.js
@@ -20,7 +20,10 @@ class App {
 	middlewares() {
 		this.app.use(_express2.default.urlencoded({ extended: true }));
 		this.app.use(_express2.default.json());
-		this.app.use(_express2.default.static(_nodepath.resolve.call(void 0, __dirname, 'uploads')));
+		this.app.use(
+			'/images/',
+			_express2.default.static(_nodepath.resolve.call(void 0, __dirname, '..', 'uploads', 'images')),
+		);
 	}
 	routes() {
 		this.app.use('/', _homeRoutes2.default);
