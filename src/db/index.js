0948befcade319a78480946ded4c18760328106c
Matheus Estevam <matheusestevam421@gmail.com>
2025-01-03 23:02:05 -0300

    mudança na db

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,13 @@
 import { Sequelize } from 'sequelize';
-import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
 import User from '../models/User';
 import Foto from '../models/Foto';
 
 const models = [Aluno, User, Foto];
 
-const connection = new Sequelize(databaseConfig);
+const connection = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres',
+});
 
 models.forEach((model) => model.init(connection));
 models.forEach(
